df7d5b392d5a48b8dcec50338633049a4366e0cd
PauMB <paumb@PauMB.local>
2018-01-05 13:58:18 +0100

    add templatedriven form

--- Forms/src/app/app.module.ts
+++ Forms/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { CommentFormComponent } from './template-driven/comment-form/comment-form.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CommentFormComponent
   ],
   imports: [
     BrowserModule
