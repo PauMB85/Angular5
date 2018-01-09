e9eb21c5e83e9335b95a1a32f2acc5b0ea6e3099
PauMB <paumb@PauMB.home>
2018-01-09 21:00:30 +0100

    add specific validations

--- Forms/src/app/template-driven/comment-form/comment-form.component.ts
+++ Forms/src/app/template-driven/comment-form/comment-form.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-comment-form',
   templateUrl: './comment-form.component.html',
@@ -10,6 +11,10 @@ export class CommentFormComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  }
+
+  log(x) {
+    console.log(x);
   }
 
 }
