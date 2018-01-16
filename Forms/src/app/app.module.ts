62519149307501deef647f0b61273ad3da1d42d1
PauMB <paumb@PauMB.local>
2018-01-16 20:20:02 +0100

    exercicies and init reactive form

--- Forms/src/app/app.module.ts
+++ Forms/src/app/app.module.ts
@@ -1,22 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
 import { CommentFormComponent } from './template-driven/comment-form/comment-form.component';
 import { AssignmentFormComponent } from './template-driven/assignment-form/assignment-form.component';
+import {SignupFormComponent} from './reactive/signup-form/signup-form.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     CommentFormComponent,
-    AssignmentFormComponent
+    AssignmentFormComponent,
+    SignupFormComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- Forms/src/app/template-driven/assignment-form/assignment-form.component.ts
+++ Forms/src/app/template-driven/assignment-form/assignment-form.component.ts
@@ -7,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AssignmentFormComponent implements OnInit {
 
+  categoryList = [
+    { id: 1, name: 'dev'},
+    { id: 2, name: 'test'}
+  ];
   constructor() { }
 
   ngOnInit() {
