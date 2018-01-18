2362409bda616a6b935081c5236dd1d82a296228
PauMB <paumb@PauMB.home>
2018-01-18 20:42:42 +0100

    create the get and post http method

---
+++ HttpServices/src/app/get-data/get-data.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+
+@Component({
+  selector: 'app-get-data',
+  templateUrl: './get-data.component.html',
+  styleUrls: ['./get-data.component.css']
+})
+export class GetDataComponent implements OnInit {
+
+  posts: any = [];
+  private  url = 'https://jsonplaceholder.typicode.com/posts';
+  constructor(private http: HttpClient) {
+    http.get(this.url, {observe: 'response'})
+      .subscribe(response => {
+        console.log(response);
+        this.posts = response.body;
+      });
+  }
+
+  ngOnInit() {
+  }
+
+  createPost(input: HTMLInputElement) {
+    const post = { title: input.value };
+    input.value = '';
+    this.http.post(this.url, JSON.stringify(post),{observe: 'response'})
+      .subscribe(response => {
+          console.log(response);
+          post['id'] = response.body;
+          this.posts.splice(0, 0, post);
+      });
+  }
+
+}
