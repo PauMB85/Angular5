44acb5d1fd0bf8e6512330e48c6ff327112600b8
PauMB <paumb85@gmail.com>
2018-01-25 12:21:06 +0100

    canActivate interface

--- auth-demo/src/app/app.module.ts
+++ auth-demo/src/app/app.module.ts
@@ -16,6 +16,7 @@ import { SignupComponent } from './signup/signup.component';
 import { AdminComponent } from './admin/admin.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { NoAccessComponent } from './no-access/no-access.component';
+import {AuthGuardService} from './services/auth-guard.service';
 
 @NgModule({
   declarations: [
@@ -33,7 +34,7 @@ import { NoAccessComponent } from './no-access/no-access.component';
     HttpModule,
     RouterModule.forRoot([
       { path: '', component: HomeComponent },
-      { path: 'admin', component: AdminComponent },
+      { path: 'admin', component: AdminComponent, canActivate: [AuthGuardService] },
       { path: 'login', component: LoginComponent },
       { path: 'no-access', component: NoAccessComponent }
     ])
@@ -41,6 +42,8 @@ import { NoAccessComponent } from './no-access/no-access.component';
   providers: [
     OrderService,
     AuthService,
+    AuthGuardService,
+    // fake server
     fakeBackendProvider,
     MockBackend,
     BaseRequestOptions
