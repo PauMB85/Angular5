6065c2096edac6118d0e127caa5fb297d168b9a3
PauMB <paumb85@gmail.com>
2018-01-25 10:54:10 +0100

    implements the login and save the token in localstorage

---
+++ auth-demo/src/app/services/auth.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
+
+@Injectable()
+export class AuthService {
+  constructor(private http: Http) {
+  }
+
+  login(credentials) {
+   return this.http.post('/api/authenticate',
+      JSON.stringify(credentials))
+     .map(response => {
+       const result = response.json();
+       if (result && result.token) {
+         // save in local storage
+         localStorage.setItem('token', result.token);
+         return true;
+       }
+       return false;
+     });
+  }
+
+  logout() {
+  }
+
+  isLoggedIn() {
+    return false;
+  }
+}
+
